b47026bd69ba010d85e45bd700a1d92579a1d94b
Marcel Kraus <git@marcelkraus.de>
2023-08-18 13:13:40 +0200

    Refactor `Contentful` code to own repository

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,49 +2,32 @@
 
 namespace App\Controller;
 
-use \DateTime;
-use Contentful\Core\Exception\NotFoundException;
-use Contentful\Delivery\Client\ClientInterface;
-use Contentful\Delivery\Query;
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends AbstractController
 {
+    private EventRepository $eventRepository;
+
+    public function __construct(EventRepository $eventRepository)
+    {
+        $this->eventRepository = $eventRepository;
+    }
+
     #[Route('/events', name: 'events')]
-    public function events(ClientInterface $client): Response
+    public function events(): Response
     {
-        try {
-            $query = new Query();
-            $query
-                ->setContentType('event')
-                ->where('fields.endDate[gte]', new DateTime())
-                ->orderBy('-fields.startingDate');
-            $events = $client->getEntries($query);
-        } catch (NotFoundException $contentfulException) {
-            throw new NotFoundHttpException();
-        }
-
         return $this->render('content/events.html.twig', [
-            'events' => $events,
+            'events' => $this->eventRepository->getAllEvents(),
         ]);
     }
 
-    public function nextEventIfApplicable(ClientInterface $client): Response
+    public function nextEventIfApplicable(): Response
     {
-        $query = new Query();
-        $query
-            ->setContentType('event')
-            ->where('fields.endDate[gte]', new DateTime())
-            ->orderBy('-fields.startingDate')
-            ->where('limit', 1);
-        $events = $client->getEntries($query);
-
         return $this->render('sidebar/next-event.html.twig', [
-            'event' => isset($events[0]) ? $events[0] : null,
+            'event' => $this->eventRepository->getNextEventOnly(),
         ]);
     }
-
 }
---
+++ src/Repository/EventRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Repository;
+
+use \DateTime;
+use Contentful\Core\Exception\NotFoundException;
+use Contentful\Core\Resource\ResourceArray;
+use Contentful\Delivery\Client\ClientInterface;
+use Contentful\Delivery\Query;
+
+class EventRepository
+{
+    private ClientInterface $client;
+
+    public function __construct(ClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    public function getAllEvents()
+    {
+        $query = $this->makeBaseQuery();
+
+        return $this->client->getEntries($query);
+    }
+
+    public function getNextEventOnly(): mixed
+    {
+        $query = $this
+            ->makeBaseQuery()
+            ->where('limit', 1)
+        ;
+
+        $events = $this->client->getEntries($query);
+
+        return isset($events[0]) ? $events[0] : null;
+    }
+
+    private function makeBaseQuery(): Query
+    {
+        $query = new Query();
+        $query
+            ->setContentType('event')
+            ->where('fields.endDate[gte]', new DateTime())
+            ->orderBy('-fields.startingDate');
+
+        return $query;
+    }
+}
