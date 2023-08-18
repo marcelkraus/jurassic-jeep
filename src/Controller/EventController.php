a51e3f09c87d9f336f66c5d3917f24faa0598d14
Marcel Kraus <git@marcelkraus.de>
2023-08-18 11:59:05 +0200

    Integrate `Contentful` to manage events list dynamically

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use \DateTime;
 use Contentful\Core\Exception\NotFoundException;
 use Contentful\Delivery\Client\ClientInterface;
+use Contentful\Delivery\Query;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends AbstractController
 {
     #[Route('/events', name: 'events')]
-    public function events(): Response
+    public function events(ClientInterface $client): Response
     {
-        return $this->render('content/events.html.twig');
+        try {
+            $query = new Query();
+            $query
+                ->setContentType('event')
+                ->where('fields.endDate[gte]', new DateTime())
+                ->orderBy('-fields.startingDate');
+
+            $events = $client->getEntries($query);
+        } catch (NotFoundException $contentfulException) {
+            throw new NotFoundHttpException();
+        }
+
+        return $this->render('content/events.html.twig', [
+            'events' => $events,
+        ]);
     }
 }
